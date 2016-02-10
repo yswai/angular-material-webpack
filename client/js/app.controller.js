b37dd72446e796bf1f821ba7456814ca497dbba4
ysw <yswai1986@gmail.com>
2016-02-10 14:34:54 +0800

    added dev server

--- client/js/app.controller.js
+++ client/js/app.controller.js
@@ -3,11 +3,16 @@ import angular from 'angular';
 var app = angular.module('app');
 app.controller('MaterialAngularCtrl', MaterialAngularCtrl);
 
-MaterialAngularCtrl.$inject = ['$mdToast'];
+MaterialAngularCtrl.$inject = ['$mdToast', '$log'];
 
-function MaterialAngularCtrl($mdToast) {
+function MaterialAngularCtrl($mdToast, $log) {
+
     var vm = this;
     vm.showSimpleToast = showSimpleToast;
+    vm.repos = loadAll();
+    vm.querySearch = querySearch;
+    vm.selectedItemChange = selectedItemChange;
+    vm.searchTextChange = searchTextChange;
 
     function showSimpleToast() {
         $mdToast.show(
@@ -17,4 +22,66 @@ function MaterialAngularCtrl($mdToast) {
                 .hideDelay(3000)
         );
     }
+
+    function querySearch(query) {
+        var results = query ? vm.repos.filter( createFilterFor(query) ) : vm.repos;
+        return results;
+    }
+    function searchTextChange(text) {
+        $log.info('Text changed to ' + text);
+    }
+    function selectedItemChange(item) {
+        $log.info('Item changed to ' + JSON.stringify(item));
+    }
+    /**
+     * Build `components` list of key/value pairs
+     */
+    function loadAll() {
+        var repos = [
+            {
+                'name'      : 'Angular 1',
+                'url'       : 'https://github.com/angular/angular.js',
+                'watchers'  : '3,623',
+                'forks'     : '16,175',
+            },
+            {
+                'name'      : 'Angular 2',
+                'url'       : 'https://github.com/angular/angular',
+                'watchers'  : '469',
+                'forks'     : '760',
+            },
+            {
+                'name'      : 'Angular Material',
+                'url'       : 'https://github.com/angular/material',
+                'watchers'  : '727',
+                'forks'     : '1,241',
+            },
+            {
+                'name'      : 'Bower Material',
+                'url'       : 'https://github.com/angular/bower-material',
+                'watchers'  : '42',
+                'forks'     : '84',
+            },
+            {
+                'name'      : 'Material Start',
+                'url'       : 'https://github.com/angular/material-start',
+                'watchers'  : '81',
+                'forks'     : '303',
+            }
+        ];
+        return repos.map( function (repo) {
+            repo.value = repo.name.toLowerCase();
+            return repo;
+        });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+        return function filterFn(item) {
+            return (item.value.indexOf(lowercaseQuery) === 0);
+        };
+    }
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,22 @@ var config = {
                     "presets": ["es2015"]
                 }
             },
+            {
+                test: /\.html$/,
+                loader: 'file?name=templates/[name]-[hash:6].html'
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'file?name=img/[name].[ext]'
+            }, {
+                test: /\.css$/,
+                loader: "style!css"
+            }, {
+                test: /\.scss$/,
+                loader: "style!css!autoprefixer!sass"
+            },{
+                test: [/fontawesome-webfont\.svg/, /fontawesome-webfont\.eot/, /fontawesome-webfont\.ttf/, /fontawesome-webfont\.woff/, /fontawesome-webfont\.woff2/],
+                loader: 'file?name=fonts/[name].[ext]'
+            },
             // {
             //     test: /\.js$/,
             //     loader: 'eslint-loader',
